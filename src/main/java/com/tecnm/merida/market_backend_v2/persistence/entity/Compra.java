081ab5a59df965b6116ec7fd3dc05bb76c35790e
Sinai-chan <chanjoana03@gmail.com>
2026-09-17 17:54:20 -0600

    feat (persistence): add Producto and Compra JPA entities

---
+++ src/main/java/com/tecnm/merida/market_backend_v2/persistence/entity/Compra.java
@@ -0,0 +1,27 @@
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name = "compras")
+public class Compra {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column (name = "id_cliente")
+    private String idCliente;
+
+    private LocalDateTime fecha;
+
+    @Column (name = "medio_pago")
+    private String medioPago;
+
+    private String comentario;
+    private String estado;
+
+
+
+}
